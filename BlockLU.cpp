38a17346bea0daeb388f7281fb7b6d9caeebcfb9
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2019-08-09 08:01:47 +0900

    re-complete

--- BlockLU.cpp
+++ BlockLU.cpp
@@ -39,6 +39,14 @@ void Show_mat(const int m, const int n, double *A)
 	cout << endl;
 }
 
+// Set all the elements of vector as Val
+void Set_vec_elements(const int m, double *b, const double Val)
+{
+//	#pragma omp parallel for
+	for (int i=0; i<m; i++)
+		b[i] = Val;
+}
+
 int main(const int argc, const char **argv)
 {
 	// Usage "a.out [size of matrix: m n ] [block width]"
@@ -55,31 +63,29 @@ int main(const int argc, const char **argv)
 	int *piv = new int[m];           // permutation vector
 
 	Gen_rand_mat(m,n,A);             // Randomize elements of orig. matrix
-//	Show_mat(m,n,A);
+	Set_vec_elements(m,b,1.0);       // Set all the elements of vec. b as 1.0
 
-//	Set_vec_elements(m,b,1.0);       // Set all the elements of vec. b as 1.0
+	//	Show_mat(m,n,A);
 
 	double timer = omp_get_wtime();
 
 	for (int i=0; i<n; i+=nb)
 	{
-		cout << "i = " << i << endl;
-
 		int ib = min(n-i,nb);
 
 		int info = LAPACKE_dgetrf2(MKL_COL_MAJOR, m-i, ib, A+(i+i*m), m, piv+i);
 		assert(info==0);
+
 		for (int k=i; k<min(m,i+ib); k++)
 			piv[k] += i;
 
 		// Apply interchanges to columns 0:i
-		LAPACKE_dlaswp(MKL_COL_MAJOR, i, A, m, i, i+ib, piv, 1);
+		LAPACKE_dlaswp(MKL_COL_MAJOR, i, A, m, i+1, i+ib, piv, 1);
 
 		if (i+ib < n)
 		{
 			// Apply interchanges to columns i+ib:n-1
-			LAPACKE_dlaswp(MKL_COL_MAJOR, n-i-ib, A+((i+ib)*m), m, i, i+ib, piv, 1);
-			cout << "n-i-ib = " << n-i-ib << ", i+ib = " << i+ib << endl;
+			LAPACKE_dlaswp(MKL_COL_MAJOR, n-i-ib, A+((i+ib)*m), m, i+1, i+ib, piv, 1);
 
 			// Compute block row of U
 			cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit,
@@ -94,10 +100,11 @@ int main(const int argc, const char **argv)
 
 	timer = omp_get_wtime() - timer;
 
-	Show_mat(m,n,A);
+//	Show_mat(m,n,A);
 	cout << "m = " << m << ", n = " << n << ", time = " << timer << endl;
 
 	delete [] A;
+	delete [] b;
 	delete [] piv;
 
 	return EXIT_SUCCESS;
