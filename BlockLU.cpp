1b9fe320c8173020be7f0310b19363cb1880447c
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2019-08-09 07:54:23 +0900

    little change

--- BlockLU.cpp
+++ BlockLU.cpp
@@ -50,15 +50,21 @@ int main(const int argc, const char **argv)
 	assert(m >= n);
 	assert(nb <= n);
 
-	double *A = new double[m*n];    // Original matrix
-	int *piv = new int[m];            // permutation vector
+	double *A = new double[m*n];   // Original matrix
+	double *b = new double[m];     // Right-hand vector
+	int *piv = new int[m];           // permutation vector
 
-	Gen_rand_mat(m,n,A);              // Randomize elements of orig. matrix
+	Gen_rand_mat(m,n,A);             // Randomize elements of orig. matrix
+//	Show_mat(m,n,A);
+
+//	Set_vec_elements(m,b,1.0);       // Set all the elements of vec. b as 1.0
 
 	double timer = omp_get_wtime();
 
 	for (int i=0; i<n; i+=nb)
 	{
+		cout << "i = " << i << endl;
+
 		int ib = min(n-i,nb);
 
 		int info = LAPACKE_dgetrf2(MKL_COL_MAJOR, m-i, ib, A+(i+i*m), m, piv+i);
@@ -67,24 +73,22 @@ int main(const int argc, const char **argv)
 			piv[k] += i;
 
 		// Apply interchanges to columns 0:i
-		info = LAPACKE_dlaswp(MKL_COL_MAJOR, i, A, m, i+1, i+ib, piv, 1);
-		assert(info==0);
+		LAPACKE_dlaswp(MKL_COL_MAJOR, i, A, m, i, i+ib, piv, 1);
 
 		if (i+ib < n)
 		{
 			// Apply interchanges to columns i+ib:n-1
-			info = LAPACKE_dlaswp(MKL_COL_MAJOR, n-i-ib, A+((i+ib)*m), m, i+1, i+ib, piv, 1);
-			assert(info==0);
+			LAPACKE_dlaswp(MKL_COL_MAJOR, n-i-ib, A+((i+ib)*m), m, i, i+ib, piv, 1);
+			cout << "n-i-ib = " << n-i-ib << ", i+ib = " << i+ib << endl;
 
 			// Compute block row of U
 			cblas_dtrsm(CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit,
 					ib, n-i-ib, 1.0, A+(i+i*m), m, A+(i+(i+ib)*m), m);
 
 			// Update trailing submatrix
-			if (i+ib < m) {
+			if (i+ib < m)
 				cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
 						m-i-ib, n-i-ib, ib, -1.0, A+(i+ib+i*m), m, A+(i+(i+ib)*m), m, 1.0, A+(i+ib+(i+ib)*m), m);
-			}
 		}
 	}
 
